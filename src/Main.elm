708ed334cff8b75cb0a551877d05eb3b66d4eefd
Michael Bylstra <mbylstra@gmail.com>
2017-07-09 11:02:37 +1000

    delete stuff

--- src/Main.elm
+++ src/Main.elm
@@ -7,26 +7,10 @@ import Navigation exposing (newUrl)
 import Pages.IssNow as IssNow
 import Pages.PeopleInSpace as PeopleInSpace
 import Taco exposing (Taco)
-
-
--- import Types exposing (Page(..), PageStatus(..))
-
 import Types exposing (Page(..))
 import LoadingStatusBar
 
 
--- Notes: should the remote data stuff really be inside each one of the models?
--- there's quite a bit of overlap between Unvisited (so what), Loading, Loaded,
--- Updating and Error. However, it could be useful if you wanted to fetch
--- multiple pages at once (prefetch)
--- Also, updating is not longer relevant
--- We need to distinguish between transitioning from and reloading a page.
--- When transitioning to we don't want to show the previous page (not sure about this).
--- When reloading, we want to continue to show the current page.
--- It's not even clear if we want to even ever bother reloading?
--- the sensible default would be to reload! Because you can easily get caching bugs.
--- So in this case, let's just clear all caches when fetching a new page.
--- TRANSITIONS
 --------------------------------------------------------------------------------
 -- Program
 --------------------------------------------------------------------------------
@@ -57,16 +41,8 @@ type alias Model =
     , peopleInSpace : PeopleInSpace.Model
 
     -- top level "private" fields -- Don't pay too much attention to these yet! Pages should not need to know about them.
-    -- , location : Navigation.Location -- we don't really need to keep this around, but it's useful for debugging
     , linkClicked : Bool
     }
-
-
-
--- TODO: figure out how to get the initial message
--- To begin with we need an init function just to get the model into a state that
--- will compile.
--- And perhaps "activate" the page? But only call that on the active page?
 
 
 init : Navigation.Location -> ( Model, Cmd Msg )
@@ -117,71 +93,10 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case (Debug.log "msg" msg) of
         UrlChanged location ->
-            if model.linkClicked then
-                -- If we got a UrlChanged msg via a link click in our app, then we are not interested. We  are
-                -- handling the state of the app. The Address bar merely reflects the current state of the app
-                -- for informational and bookmarking purposes. (It is not the source of truth for state)
-                { model | linkClicked = False } ! []
-            else
-                -- Seeing as we are using HTML5 history for clean urls, and we know that
-                -- a link wasn't clicked, we can deduce that the back or forward button
-                -- was clicked. How do we get the state? Seeing as the Elm Navigation does
-                -- not support serializing state when calling pushState (it should, but it doesn't)
-                -- and we haven't implementation our own custom history API in elm (this would be possible),
-                -- all we have to work with is the URL and to deduce as much state as we
-                -- can from the URL. Depending on how much state we choose to store
-                -- in the url we may not be able to bring the page to exactly the
-                -- same state the page was in.
-                (model
-                    |> updatePage (parseLocation location)
-                    |> setLinkClicked False
-                )
-                    ! []
+            handleUrlChanged location model
 
         RouteChangeRequested page ->
-            let
-                newUrlCmd =
-                    newUrl (getUrl page)
-
-                reloading =
-                    page == model.taco.currentPage
-
-                ( model2, cmd ) =
-                    case page of
-                        PeopleInSpacePage ->
-                            let
-                                f =
-                                    if reloading then
-                                        PeopleInSpace.reload
-                                    else
-                                        PeopleInSpace.navigateTo
-
-                                ( newPeopleInSpace, pisMsg ) =
-                                    f model.peopleInSpace
-                            in
-                                { model | peopleInSpace = newPeopleInSpace } ! [ Cmd.map PeopleInSpaceMsg pisMsg ]
-
-                        IssNowPage ->
-                            let
-                                f =
-                                    if reloading then
-                                        IssNow.reload
-                                    else
-                                        IssNow.navigateTo
-
-                                ( newIssNow, issNowMsg ) =
-                                    f model.issNow
-                            in
-                                { model | issNow = newIssNow } ! [ Cmd.map IssNowMsg issNowMsg ]
-
-                        NotFound ->
-                            model ! [ Cmd.none ]
-            in
-                (model2
-                    |> updatePage page
-                    |> setLinkClicked True
-                )
-                    ! [ newUrlCmd, cmd ]
+            handleRouteChangeRequested page model
 
         PeopleInSpaceMsg pisMsg ->
             { model | peopleInSpace = PeopleInSpace.update pisMsg model.peopleInSpace } ! []
@@ -190,36 +105,78 @@ update msg model =
             { model | issNow = IssNow.update issNowMsg model.issNow } ! []
 
 
+handleUrlChanged : Navigation.Location -> Model -> ( Model, Cmd Msg )
+handleUrlChanged location model =
+    if model.linkClicked == True then
+        -- If we got a UrlChanged msg via a link click in our app, then we are not interested. We  are
+        -- handling the state of the app. The Address bar merely reflects the current state of the app
+        -- for informational and bookmarking purposes. (It is not the source of truth for state)
+        -- Reset linkClicked to False so we can detect
+        { model | linkClicked = False } ! []
+    else
+        -- Seeing as we are using HTML5 history for clean urls, and we know that
+        -- a link wasn't clicked, we can deduce that the back or forward button
+        -- was clicked. How do we get the state? Seeing as the Elm Navigation does
+        -- not support serializing state when calling pushState (it should, but it doesn't)
+        -- and we haven't implementation our own custom history API in elm (this would be possible),
+        -- all we have to work with is the URL and to deduce as much state as we
+        -- can from the URL. Depending on how much state we choose to store
+        -- in the url we may not be able to bring the page to exactly the
+        -- same state the page was in.
+        (model
+            |> updatePage (parseLocation location)
+        )
+            ! []
 
--- case result of
---     Ok peopleData ->
---         (model
---             |> setPageStatus Loaded
---             |> setPeopleInSpacePeople peopleData
---         )
---             ! []
---
---     Err httpError ->
---         (model
---             |> setPageStatus (DataFetchError <| toString httpError)
---         )
---             ! []
--- case result of
---     Ok peopleData ->
---         let
---             _ =
---                 ""
---
---             -- newPeople =
---             --     Just people
---             -- newPeopl
---         in
---             -- The interesting thing here is that we want to keep the current local state,
---             -- so wrapping it in a maybe doesn't work! The data itself needs to be in a maybe.
---             { model | maybePeopleInSpacePageData = Just peopleData, pageStatus = Loaded } ! []
---
---     Err httpError ->
---         { model | pageStatus = DataFetchError <| toString httpError } ! []
+
+handleRouteChangeRequested : Page -> Model -> ( Model, Cmd Msg )
+handleRouteChangeRequested page model =
+    let
+        newUrlCmd =
+            newUrl (getUrl page)
+
+        reloading =
+            page == model.taco.currentPage
+
+        ( model2, cmd ) =
+            case page of
+                PeopleInSpacePage ->
+                    let
+                        f =
+                            if reloading then
+                                PeopleInSpace.reload
+                            else
+                                PeopleInSpace.navigateTo
+
+                        ( newPeopleInSpace, pisMsg ) =
+                            f model.peopleInSpace
+                    in
+                        { model | peopleInSpace = newPeopleInSpace } ! [ Cmd.map PeopleInSpaceMsg pisMsg ]
+
+                IssNowPage ->
+                    let
+                        f =
+                            if reloading then
+                                IssNow.reload
+                            else
+                                IssNow.navigateTo
+
+                        ( newIssNow, issNowMsg ) =
+                            f model.issNow
+                    in
+                        { model | issNow = newIssNow } ! [ Cmd.map IssNowMsg issNowMsg ]
+
+                NotFound ->
+                    model ! [ Cmd.none ]
+    in
+        (model2
+            |> updatePage page
+            |> setLinkClicked True
+        )
+            ! [ newUrlCmd, cmd ]
+
+
+
 --------------------------------------------------------------------------------
 -- View
 --------------------------------------------------------------------------------
@@ -229,12 +186,9 @@ view : Model -> Html Msg
 view model =
     div
         []
-        -- [ statusBar model.taco.pageStatus
         [ loadingBar model
         , topNav
         , pageView model
-
-        -- , text <| toString model
         ]
 
 
@@ -259,94 +213,24 @@ loadingBar model =
             text "Not Found"
 
 
-
--- statusBar : PageStatus -> Html Msg
--- statusBar pageStatus =
---     case pageStatus of
---         LoadingFirstPage ->
---             div []
---                 [ text "loading first page"
---                 ]
---
---         -- can we pass the view and the data, and choose whether to pass in the
---         -- data or just nothing?
---         LoadingNextPageData ->
---             div []
---                 [ text "loading new page"
---                 ]
---
---         Reloading ->
---             div []
---                 [ text "reloading"
---                 ]
---
---         -- Transitioning ->
---         --     div []
---         --         []
---         Loaded ->
---             div []
---                 []
---
---         DataFetchError error ->
---             div []
---                 [ text <| toString error ]
-
-
 pageView : Model -> Html Msg
 pageView model =
     let
         taco =
             model.taco
-
-        -- showCachedData =
-        --     figureOutWhetherToShowCachedData taco.pageStatus
     in
         case taco.currentPage of
             IssNowPage ->
-                -- case showCachedData of
-                -- True ->
                 IssNow.view model.issNow
 
-            -- False ->
-            --     IssNow.view Nothing
             PeopleInSpacePage ->
-                -- case showCachedData of
-                -- True ->
                 PeopleInSpace.view model.peopleInSpace
 
-            -- False ->
-            --     PeopleInSpace.view model.peopleInSpace
-            -- False ->
-            --     PeopleInSpace.view Nothing
             NotFound ->
                 div [] [ text "Not Found" ]
 
 
 
---------------------------------------------------------------------------------
--- SPA Routing
---------------------------------------------------------------------------------
--- figureOutWhetherToShowCachedData : PageStatus -> Bool
--- figureOutWhetherToShowCachedData pageStatus =
---     case pageStatus of
---         LoadingFirstPage ->
---             False
---
---         -- can we pass the view and the data, and choose whether to pass in the
---         -- data or just nothing?
---         LoadingNextPageData ->
---             False
---
---         Reloading ->
---             True
---
---         -- Transitioning ->
---         --     False
---         Loaded ->
---             True
---
---         DataFetchError error ->
---             True
 --------------------------------------------------------------------------------
 -- Model Setter Boilerplate
 -- Hopefully one day there will be an Elm syntax for this :)
@@ -365,24 +249,3 @@ updatePage page model =
 setLinkClicked : Bool -> Model -> Model
 setLinkClicked b model =
     { model | linkClicked = b }
-
-
-
--- setPageStatus : PageStatus -> Model -> Model
--- setPageStatus pageStatus model =
---     let
---         taco =
---             model.taco
---     in
---         { model | taco = { taco | pageStatus = pageStatus } }
--- setMaybeIssNowPageData : Maybe IssNow.Model -> Model -> Model
--- setMaybeIssNowPageData v m =
---     { m | maybeIssNowPageData = v }
--- setPeopleInSpacePeople : PeopleInSpace.People -> Model -> Model
--- setPeopleInSpacePeople people model =
---     { model | peopleInSpace = PeopleInSpace.setPeople people model.peopleInSpace }
-
-
-setPeopleInSpace : PeopleInSpace.Model -> Model -> Model
-setPeopleInSpace v m =
-    { m | peopleInSpace = v }
